ea80027ce28d0640ebf196a6da45ce1e9e3c13cc
Jake Oliver <jeoliver97@gmail.com>
2021-04-13 21:29:17 -0700

    Add files via upload

---
+++ webapp/README.md
@@ -0,0 +1,11 @@
+# Finance Automation Project
+
+Currently in progress
+
+
+## Potential Goals for Project
+Display monthly budget and monthly spending/income side-by-side  
+Display total spending per month over the course of a year  
+Display total spending on certain budget categories and certain locations  
+	Toggle between including & excluding certain categories/locations
+
---
+++ webapp/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a05b81fb676763d2a5f7725c12bea17e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from webapp import routes
